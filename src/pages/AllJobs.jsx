50552f0c25626ab2d8f5165f4c9905cf62546154
Tonmoy <tonmoysutradhar2002@gmail.com>
2024-12-21 17:08:45 +0600

    Reset button done in alljobs page.

--- src/pages/AllJobs.jsx
+++ src/pages/AllJobs.jsx
@@ -36,7 +36,11 @@ const AllJobs = () => {
     fetchAllJobs();
   }, [filter, search, sort]);
 
-  console.log(filter);
+  const handleReset = () => {
+    setFilter("");
+    setSort("");
+    setSearch("");
+  };
   return (
     <div className="container px-6 py-10 mx-auto min-h-[calc(100vh-306px)] flex flex-col justify-between">
       <div>
@@ -47,6 +51,7 @@ const AllJobs = () => {
               id="category"
               className="border p-4 rounded-lg"
               onChange={(e) => setFilter(e.target.value)}
+              value={filter}
             >
               <option value="">Filter By Category</option>
               <option value="Web Development">Web Development</option>
@@ -63,6 +68,7 @@ const AllJobs = () => {
               name="search"
               // For search we can user onChange || onBlur
               onBlur={(e) => setSearch(e.target.value)}
+              // value={search}
               placeholder="Enter Job Title"
               aria-label="Enter Job Title"
             />
@@ -78,13 +84,16 @@ const AllJobs = () => {
               id="category"
               className="border p-4 rounded-md"
               onChange={(e) => setSort(e.target.value)}
+              value={sort}
             >
               <option value="">Sort By Deadline</option>
               <option value="dsc">Descending Order</option>
               <option value="asc">Ascending Order</option>
             </select>
           </div>
-          <button className="btn">Reset</button>
+          <button onClick={handleReset} className="btn">
+            Reset
+          </button>
         </div>
         <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-16 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
           {jobs.map((job, indx) => (
