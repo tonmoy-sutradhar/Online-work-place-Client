f135c39f7d9bb45ffd5ab0e6118af5eb1c2acb69
Tonmoy <tonmoysutradhar2002@gmail.com>
2024-12-21 16:41:12 +0600

    search by text done.

--- src/pages/AllJobs.jsx
+++ src/pages/AllJobs.jsx
@@ -5,16 +5,35 @@ import axios from "axios";
 const AllJobs = () => {
   const [jobs, setJobs] = useState([]);
   const [filter, setFilter] = useState("");
+  const [search, setSearch] = useState("");
+  // useEffect(() => {
+  //   const fetchAllJobs = async () => {
+  //     const { data } = await axios.get(
+  //       // ?filter=${filter} filter er value jodi change hoi tahole jeta dice oita set koren.
+  //       `${
+  //         import.meta.env.VITE_API_URL
+  //       }/all-jobs?filter=${filter}& search=${search}`
+  //     );
+  //     setJobs(data);
+  //   }
+  //   fetchAllJobs();
+  // }, [filter, search]);
+
   useEffect(() => {
     const fetchAllJobs = async () => {
-      const { data } = await axios.get(
-        // ?filter=${filter} filter er value jodi change hoi tahole jeta dice oita set koren.
-        `${import.meta.env.VITE_API_URL}/all-jobs?filter=${filter}`
-      );
-      setJobs(data);
+      try {
+        const { data } = await axios.get(
+          `${
+            import.meta.env.VITE_API_URL
+          }/all-jobs?filter=${filter}&search=${search}`
+        );
+        setJobs(data);
+      } catch (error) {
+        console.error("Error fetching jobs:", error);
+      }
     };
     fetchAllJobs();
-  }, [filter]);
+  }, [filter, search]);
 
   console.log(filter);
   return (
@@ -35,21 +54,23 @@ const AllJobs = () => {
             </select>
           </div>
 
-          <form>
-            <div className="flex p-1 overflow-hidden border rounded-lg    focus-within:ring focus-within:ring-opacity-40 focus-within:border-blue-400 focus-within:ring-blue-300">
-              <input
-                className="px-6 py-2 text-gray-700 placeholder-gray-500 bg-white outline-none focus:placeholder-transparent"
-                type="text"
-                name="search"
-                placeholder="Enter Job Title"
-                aria-label="Enter Job Title"
-              />
+          {/* <form> */}
+          <div className="flex p-1 overflow-hidden border rounded-lg    focus-within:ring focus-within:ring-opacity-40 focus-within:border-blue-400 focus-within:ring-blue-300">
+            <input
+              className="px-6 py-2 text-gray-700 placeholder-gray-500 bg-white outline-none focus:placeholder-transparent"
+              type="text"
+              name="search"
+              // For search we can user onChange || onBlur
+              onBlur={(e) => setSearch(e.target.value)}
+              placeholder="Enter Job Title"
+              aria-label="Enter Job Title"
+            />
 
-              <button className="px-1 md:px-4 py-3 text-sm font-medium tracking-wider text-gray-100 uppercase transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:bg-gray-600 focus:outline-none">
-                Search
-              </button>
-            </div>
-          </form>
+            <button className="px-1 md:px-4 py-3 text-sm font-medium tracking-wider text-gray-100 uppercase transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:bg-gray-600 focus:outline-none">
+              Search
+            </button>
+          </div>
+          {/* </form> */}
           <div>
             <select
               name="category"
